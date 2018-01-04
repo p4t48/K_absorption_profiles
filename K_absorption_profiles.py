3810ddab741b8f3298d99925ee0716f9802bdf4a
Peter Koss <peter.koss@kuleuven.be>
2018-01-04 12:20:07 +0100

    Added function PeakDet which is adapted from https://gist.github.com/endolith/250860

--- K_absorption_profiles.py
+++ K_absorption_profiles.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import glob
+from scipy.signal import find_peaks_cwt
+import sys
 
 
 class KAbsProfiles:
@@ -27,10 +29,60 @@ class KAbsProfiles:
         else:
             print("Needs to be either 16 or 32 bit!")
 
+        # Get voltage values for channel data
         self.channel1 = self.data[channelLayout['ch1']-1::4] * channel1Norm            
         self.channel2 = self.data[channelLayout['ch2']-1::4] * channel2Norm
         self.channel3 = self.data[channelLayout['ch3']-1::4] * channel3Norm
         self.channel4 = self.data[channelLayout['ch4']-1::4] * channel4Norm
+
+
+    def PeakDet(self, data, delta, indices = None):
+        """ Function adapted from https://gist.github.com/endolith/250860.
+            Returns the maxima and minima with gap delta of an array."""
+        
+        maxima, minima = [], []
+        
+        if indices is None:
+            indices = np.arange(len(data))
+
+        data = np.asarray(data)
+    
+        if len(data) != len(indices):
+            sys.exit('Input vectors data and indices must have same length')
+        
+        if not np.isscalar(delta):
+            sys.exit('Input argument delta must be a scalar')
+        
+        if delta <= 0:
+            sys.exit('Input argument delta must be positive')
+        
+        mn, mx = np.Inf, -np.Inf
+        mnpos, mxpos = np.NaN, np.NaN
+        lookformax = True
+    
+        for i in np.arange(len(data)):
+            this = data[i]
+            if this > mx:
+                mx = this
+                mxpos = indices[i]
+            if this < mn:
+                mn = this
+                mnpos = indices[i]
+
+            if lookformax:
+                if this < mx-delta:
+                    maxima.append(mxpos)
+                    mn = this
+                    mnpos = indices[i]
+                    lookformax = False
+            else:
+                if this > mn+delta:
+                    minima.append(mnpos)
+                    mx = this
+                    mxpos = indices[i]
+                    lookformax = True
+
+        return np.array(maxima), np.array(minima)
 
     def PlotChannelData(self, channel):
 
@@ -44,10 +96,22 @@ class KAbsProfiles:
             data = self.channel4
 
         xVals = np.linspace(0,1,len(data))
-
+        maximaLocations, minimaLocations = self.PeakDet(data, 0.1)
+        print(maximaLocations)
+        print(minimaLocations)
+        
+        xValsMax = np.take(xVals, maximaLocations)
+        xValsMin = np.take(xVals, minimaLocations)
+        maxima = np.take(data, maximaLocations)
+        minima = np.take(data, minimaLocations)
+        print(maxima.size)
+        print(xValsMax.size)
+                
         plt.plot(xVals, data)
+        plt.plot(xValsMax, maxima, 'ro')
+        plt.plot(xValsMin, minima, 'ko')
         plt.show()
-            
+
 #
 # Analyse data
 #
