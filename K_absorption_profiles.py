96df73e296faa0062843e84afdc1e8dc91338537
Peter Koss <peter.koss@kuleuven.be>
2018-01-05 18:27:28 +0100

    replaced function mz_fwt with t_scan as it finds mode hops more reliably. Parameter estimations for gaussians should now be sound

--- K_absorption_profiles.py
+++ K_absorption_profiles.py
@@ -13,6 +13,9 @@ class KAbsProfiles:
         self.samplingRate = samplingRate
         self.amplifierGains = amplifierGains
 
+        # Place holder for absorption data
+        self.absorptionProfile = None
+        
         # Depending on the data format, get signal in volts
         f = open("%s" % self.dataFile, "r")
         if bits == 16:
@@ -101,7 +104,7 @@ class KAbsProfiles:
         elif channel == 4:
             data = self.channel4[absStart:absStop]
         else:
-            pass
+            print("Need data from some channel!")
 
         if plot == True:
             xVals = np.linspace(0,1,data.size)
@@ -126,22 +129,22 @@ class KAbsProfiles:
         elif channel == 4:
             data = self.channel4[absStart:absStop]
         else:
-            pass
+            print("Need data from some channel!")
 
         # Finding where to cut the data because of mode-hops or other
-        jumps = sd.mz_fwt(data, n=2)
+        jumps  = np.abs(sd.t_scan(data, window=10))
         jumps /= np.abs(jumps).max()
-        jumpLocations = np.where(jumps >= 1e-4)[0] # 1e-4 seems to properly discriminate mode-hops
+        jumpLocations = np.where(jumps >= 0.8)[0] # 0.8 seems to properly discriminate mode-hops
 
         # Cutting data from the last mode-hop on
-        dataCut = data[jumpLocations.max()+100:]
-        maximaLocations, minimaLocations = self.PeakDet(data, 0.1)
+        dataCut = data[jumpLocations[-1]+100:]
+        maximaLocations, minimaLocations = self.PeakDet(data, 0.01)
         
         if plot == True:
             
             # Plotting all relevant points and the data
             xVals = np.linspace(0,1,data.size)
-            xValsCut = xVals[jumpLocations.max()+100:]
+            xValsCut = xVals[jumpLocations[-1]+100:]
             xValsMinima = np.take(xVals, minimaLocations)            
             xValsJumps = np.take(xVals, jumpLocations)
             jumpVals = np.take(data, jumpLocations)
@@ -154,6 +157,7 @@ class KAbsProfiles:
             plt.legend()
             plt.show()
 
+        self.absorptionProfile = dataCut
         return dataCut
     
     def PlotChannelData(self, channel):
@@ -171,3 +175,20 @@ class KAbsProfiles:
         xVals = np.linspace(0,1,len(data))                
         plt.plot(xVals, data)
         plt.show()
+
+    def AbsorptionParameters(self, N, channel):
+        """ Crude estimation of initial parameters for the fitting routine. """
+        
+        self.AbsorptionSignal(N, channel)
+        maximaLocations, minimaLocations = self.PeakDet(self.absorptionProfile, 0.01)
+        minimaValues = np.take(self.absorptionProfile, minimaLocations)
+
+        gaussianMean = minimaLocations[minimaValues.argmin()]
+        gaussianStd = self.absorptionProfile.size/3
+        laserPower = np.mean(self.absorptionProfile)
+        gaussianAmplitude = np.abs(laserPower - np.take(self.absorptionProfile, gaussianMean))
+        scanSlope = (self.absorptionProfile[-1] - self.absorptionProfile[0]) / self.absorptionProfile.size
+
+        fitGuesses = {"mean": gaussianMean, "std": gaussianStd, "amplitude": gaussianAmplitude, "DC": laserPower, "scan": scanSlope}
+
+        return fitGuesses
--- analysis.py
+++ analysis.py
@@ -13,9 +13,11 @@ channelRanges = {'ch1': 10, 'ch2': 10, 'ch3': 10, 'ch4': 10}
 amplifierGains = {'ch1': 10**6, 'ch2': 10**6, 'ch3': 1, 'ch4': 1}
 
 # Data files location        
-dataFiles = glob.glob("../20171222/*_7_*")
+dataFiles = glob.glob("../20171218/*_0.5_probe")
 print(dataFiles)
 
 # Work on the data
-an = KAbsProfiles(dataFiles[1], samplingRate, bits, channelLayout, channelRanges, amplifierGains)
-an.AbsorptionSignal(5,2,True)
+an = KAbsProfiles(dataFiles[0], samplingRate, bits, channelLayout, channelRanges, amplifierGains)
+#an.PlotChannelData(2)
+an.AbsorptionSignal(1,2,True)
+print(an.AbsorptionParameters(1,2))
